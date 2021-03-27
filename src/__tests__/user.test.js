b4db61dbc05b1996ac54fddbffe3971027ee4608
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-03-27 22:42:29 +0300

    first commit

---
+++ src/__tests__/user.test.js
@@ -0,0 +1,40 @@
+import attackOption from '../js/user';
+
+const character = {
+  name: 'Лучник',
+  type: 'Bowman',
+  health: 50,
+  level: 3,
+  attack: 40,
+  defence: 10,
+  special: [
+    {
+      id: 8,
+      name: 'Двойной выстрел',
+      icon: 'http://...',
+      description: 'Двойной выстрел наносит двойной урон',
+    },
+    {
+      id: 9,
+      name: 'Нокаутирующий удар',
+      icon: 'http://...',
+    },
+  ],
+};
+
+test('function will return correct array', () => {
+  const referance = [{
+    id: 8,
+    name: 'Двойной выстрел',
+    icon: 'http://...',
+    description: 'Двойной выстрел наносит двойной урон',
+  },
+  {
+    id: 9,
+    name: 'Нокаутирующий удар',
+    icon: 'http://...',
+    description: 'Описание недоступно',
+  },
+  ];
+  expect(attackOption(character)).toEqual(referance);
+});
